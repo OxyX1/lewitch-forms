bcb9cbffa52db6ac8e99828c6f5b0fccdce22155
oxyum services <29GariglioSe@fortcherry.org>
2025-03-03 22:23:23 -0500

    push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,18 +14,18 @@ const io = socketIo(server, {
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'));
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login", "index.html"));
 });
 
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dashboard', 'index.html'));
+app.get("/dashboard", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "dashboard", "index.html"));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const users = {}; // Store connected users
@@ -34,7 +34,10 @@ io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
     socket.on("user join", (username) => {
-        console.log(`${username} joined the chat`);
+        users[socket.id] = username;
+        io.emit("chat message", { user: "System", message: `${username} has joined the chat.` });
+        io.emit("update users", Object.values(users));
+        console.log(`${username} joined the chat.`);
     });
 
     socket.on("chat message", (data) => {
@@ -42,19 +45,12 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        console.log("A user disconnected:", socket.id);
-    });
-});
-
-
-    // When a user disconnects
-    socket.on('disconnect', () => {
         const username = users[socket.id];
         if (username) {
-            io.emit('chat message', { user: 'System', message: `${username} has left the chat.` });
+            io.emit("chat message", { user: "System", message: `${username} has left the chat.` });
         }
         delete users[socket.id];
-        io.emit('update users', Object.values(users));
+        io.emit("update users", Object.values(users));
         console.log(`User disconnected: ${socket.id}`);
     });
 });
