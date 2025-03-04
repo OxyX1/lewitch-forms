4aa53a8c74e53eb2d06d60f8b1a0a78c335e3c81
oxyum services <29GariglioSe@fortcherry.org>
2025-03-03 20:28:03 -0500

    push

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'login')));
-app.use(express.static(path.join(__dirname, 'public', 'dashboard')));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
@@ -23,14 +40,6 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dashboard', 'index.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'))
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
